1cf158bc22b41393f96758bca60091faf67059be
wpcodevo <ziddah.edem@gmail.com>
2023-03-30 17:38:25 +0000

    updated

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ mod model;
 mod response;
 mod route;
 
-use axum::http::{HeaderValue, Method};
+use axum::http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+};
 use route::create_router;
 use tower_http::cors::CorsLayer;
 
@@ -12,7 +15,8 @@ async fn main() {
     let cors = CorsLayer::new()
         .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
-        .allow_credentials(true);
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
     let app = create_router().layer(cors);
 
