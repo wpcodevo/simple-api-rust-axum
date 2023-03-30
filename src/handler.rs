9dbb6a49d647f6216d4e8d19a69d03fbeff41721
wpcodevo <ziddah.edem@gmail.com>
2023-03-30 11:28:18 +0000

    updated

--- src/handler.rs
+++ src/handler.rs
@@ -12,7 +12,7 @@ use crate::{
 };
 
 pub async fn health_checker_handler() -> impl IntoResponse {
-    const MESSAGE: &str = "Build Simple CRUD API with Rust";
+    const MESSAGE: &str = "Build Simple CRUD API in Rust using Axum";
 
     let json_response = serde_json::json!({
         "status": "success",
